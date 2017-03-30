58bf61a8769df2e7e53e814279ee88b60e00b8c1
Damien Cathrine <damien.cathrine@phelma.grenoble-inp.fr>
2017-03-30 21:48:54 +0200

    Ajout fonction allocation mem pour tab 2 dim

--- src/genBST.c
+++ src/genBST.c
@@ -1,5 +1,45 @@
 #include "genBST.h"
 
 void genBST(int n, int *values){
-    // TO COMPLETE
+    int** bst = allocate2D(n, 2);
+    assert(bst != NULL);
+    print_BST(n, bst);
+    desallocate2D(bst);
+}
+
+int ** allocate2D(int nb_lines, int nb_columns){
+    int **p = NULL;
+
+    // Allocation the adresses of lines
+    p = (int **)calloc(nb_lines, sizeof(*p));
+    if (p == NULL) {
+        return NULL;
+    }
+    else {
+        // Allocating the data array
+        *p = (int *)calloc(nb_lines * nb_columns, sizeof(**p));
+        if (*p == NULL) {
+            free(p);
+            return NULL;
+        }
+        else {
+            for (size_t i = 1; i < nb_lines; i++) {
+                p[i] = p[i-1] + nb_columns;
+            }
+        }
+    }
+    return p;
+}
+
+void desallocate2D(int **array){
+    free(*array);
+    free(array);
+}
+
+void print_BST_line(int n, int **bst){
+    printf("[");
+    for (size_t i = 0; i < n; i++) {
+        printf("[%d,%d]", bst[i][0], bst[i][1]);
+    }
+    printf("]\n");
 }
--- src/genBST.h
+++ src/genBST.h
@@ -8,6 +8,30 @@
 #include <assert.h>
 #include <string.h>
 
+/**
+ * Main function calling the calculus function
+ * @param n number of values
+ * @param values array of values
+ */
 void genBST(int n, int *values);
+
+/**
+ * Function allocating memory for a 2D array
+ * the data array is contigous
+ */
+int ** allocate2D(int nb_lines, int nb_columns);
+
+/**
+ * Function freeing memory previously allocated
+ * with allocate2D
+ */
+void desallocate2D(int **array);
+
+/**
+ * Function basically printing the BST
+ * @param n number of values
+ * @param bst tree [[-1,-1],[0,2],[-1,-1]]
+ */
+void print_BST_line(int n, int **bst);
 
 #endif // __GENBST_H__
